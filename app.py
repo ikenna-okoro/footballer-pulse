6214f1a6f7c9cfd0a92f4bd464daa07a6e83f244
ikenna <ike_genius1@yahoo.com>
2025-04-30 19:49:51 +0100

    milestone 1 - GET request  endpoint displayed in Postman, and effected corrections

--- app.py
+++ app.py
@@ -1,3 +1,23 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, jsonify
 from flask_session import Session
+
+# Configure the app
+app = Flask(__name__)
+
+# Hardcoded player details
+player_data = {
+    "id": 1,
+    "name": "Roberto Carlos",
+    "age": 50,
+    "t_shirt_number": 3,
+    "position": "Defender",
+}
+
+# Endpoint to return a hardcoded player details.
+@app.route("/players", methods=["GET"])
+def getAllPlayers():
+    return jsonify(player_data)
+
+
+
 
