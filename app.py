7ff6d32ec63147c27e2bd3205d40835cf50dc235
ikenna <ike_genius1@yahoo.com>
2025-05-06 23:27:31 +0100

    Milestone 2 with free third party service to retrieve player names given a specific name in the endpoint.

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, redirect, render_template, request, jsonify
+from flask import Flask, redirect, render_template, jsonify
+import requests
+import os
+from dotenv import load_dotenv
+from pprint import pprint
 from flask_session import Session
 
 # Configure the app
@@ -16,6 +20,26 @@ player_data = {
 # Endpoint to return a hardcoded player details.
 @app.route("/players", methods=["GET"])
 def getAllPlayers():
+    return jsonify(player_data)
+
+@app.route("/players/test", methods=["GET"])
+def getAllPlayersTest():
+
+    url = "https://v3.football.api-sports.io/players/profiles?player=276"
+
+    payload={}
+    load_dotenv()
+    headers = {
+    'x-rapidapi-key': os.getenv("API_KEY"),
+    'x-rapidapi-host': 'v3.football.api-sports.io'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    pprint(response.json())
+
+
+    
     return jsonify(player_data)
 
 
